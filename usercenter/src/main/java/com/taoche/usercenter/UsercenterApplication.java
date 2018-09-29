35a7b307c4fde073e0944b05ae6f0a9dd408e7d4
guohuawei <guohuawei@yxqiche.com>
2018-09-29 23:33:34 +0800

    init

---
+++ usercenter/src/main/java/com/taoche/usercenter/UsercenterApplication.java
@@ -0,0 +1,14 @@
+package com.taoche.usercenter;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercenterApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UsercenterApplication.class, args);
+	}
+}
